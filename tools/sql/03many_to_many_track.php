b57005294721e83cb0f27ccd1c135d502b4a3154
csev <csev@umich.edu>
2019-10-30 21:01:09 +0000

    MVP track-many-to-many

---
+++ tools/sql/03many_to_many_track.php
@@ -0,0 +1,203 @@
+<?php
+
+use \Tsugi\Core\LTIX;
+use \Tsugi\Util\LTI;
+use \Tsugi\Util\PDOX;
+use \Tsugi\Util\U;
+use \Tsugi\Util\Mersenne_Twister;
+
+$MAX_UPLOAD_FILE_SIZE = 1024*1024;
+
+require_once "sql_util.php";
+
+if ( ! pg4e_user_db_load($LAUNCH) ) return;
+
+$file = fopen("library.csv","r");
+$library = array();
+$answer = array();
+while ( $pieces = fgetcsv($file) ) {
+    $library[] = $pieces;
+    $answer[] = array($pieces[0], $pieces[2], $pieces[1]);
+}
+fclose($file);
+
+sort($library);
+sort($answer);
+
+$sql = "SELECT track.title, album.title, artist.name
+FROM track
+JOIN album ON track.album_id = album.id
+JOIN tracktoartist ON track.id = tracktoartist.track_id
+JOIN artist ON tracktoartist.artist_id = artist.id
+ORDER BY track.title
+LIMIT 3;";
+
+$oldgrade = $RESULT->grade;
+
+if ( U::get($_POST,'check') ) {
+
+    $pg_PDO = pg4e_get_user_connection($LAUNCH, $pdo_connection, $pdo_user, $pdo_pass);
+    if ( ! $pg_PDO ) return;
+    if ( ! pg4e_check_debug_table($LAUNCH, $pg_PDO) ) return;
+
+    $stmt = pg4e_query_return_error($pg_PDO, $sql);
+    if ( ! $stmt ) return;
+
+    $good = 0;
+    $pos = 0;
+    $error = false;
+    $found = false;
+    while ($row = $stmt->fetch(\PDO::FETCH_NUM)) {
+        $ans = $answer[$pos];
+        foreach($ans as $i => $txt ) {
+            if ($row[$i] != $txt ) {
+                $pos++; $col = $i + 1;
+                $_SESSION['error'] = "Row $pos column $col expected $txt, got ".$row[$i];
+                $error = true;
+                break;
+            }
+        }
+        if ( $error ) break;
+        $good++;
+        $pos++;
+    }
+
+    if ( $pos == 0 ) {
+        $_SESSION['error'] = "No records found in tracks table";
+        $error = true;
+    }
+
+    if ( $good < 3 ) {
+        header( 'Location: '.addSession('index.php') ) ;
+        return;
+    }
+
+    $gradetosend = 1.0;
+    pg4e_grade_send($LAUNCH, $pg_PDO, $oldgrade, $gradetosend, $dueDate);
+
+    // Redirect to ourself
+    header('Location: '.addSession('index.php'));
+    return;
+}
+
+if ( $RESULT->grade > 0 ) {
+    echo('<p class="alert alert-info">Your current grade on this assignment is: '.($RESULT->grade*100.0).'%</p>'."\n");
+}
+
+if ( $dueDate->message ) {
+    echo('<p style="color:red;">'.$dueDate->message.'</p>'."\n");
+}
+?>
+<h1>Musical Track Database plus Artists</h1>
+<p>
+This application will read an iTunes library in comma-separated-values (CSV)
+and produce a properly normalized database as specified below.
+Once you have placed the proper data in the database, press the button below to
+check your answer.
+</p>
+<?php pg4e_user_db_form($LAUNCH); ?>
+<p>
+We will do some things differently in this assignment.   We will not use
+a separate "raw" table, we will just use <b>ALTER TABLE</b> statements
+to remove coliumns after we don't need them
+(i.e. we converted them into foreign keys).
+</p>
+<p>
+We will use the same 
+<a href=library.csv" target="_blank">
+CSV track data
+</a>
+as in prior exercises - this time we will build a many-to-many relationship
+using a <i>junction/through/ join</i> table between tracks and artists.
+
+<p>
+To grade this assignment, the program will run a query like this on
+your database and look for the data it expects to see:
+<pre>
+<?= htmlentities($sql) ?>
+</pre>
+The expected result of this query on your database is:
+<table border="2">
+<tr>
+<th>track</th><th>album</th><th>artist</th>
+</tr>
+<?php
+$pos=0;
+foreach($answer as $ans) {
+    echo("<tr>");
+    foreach($ans as $i => $txt ) {
+        echo("<td>".htmlentities($txt)."</td>");
+    }
+    echo("<tr>\n");
+    $pos++;
+    if ( $pos >= 3 ) break;
+}
+?>
+</table>
+</p>
+<p>
+In this we will give you a partial script with portions of some of the commands replaced by
+thre dots... 
+<pre>
+DROP TABLE album CASCADE;
+CREATE TABLE album (
+    id SERIAL,
+    title VARCHAR(128) UNIQUE,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE track CASCADE;
+CREATE TABLE track (
+    id SERIAL,
+    title TEXT, 
+    artist TEXT, 
+    album TEXT, 
+    album_id INTEGER REFERENCES album(id) ON DELETE CASCADE,
+    count INTEGER, 
+    rating INTEGER, 
+    len INTEGER,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE artist CASCADE;
+CREATE TABLE artist (
+    id SERIAL,
+    name VARCHAR(128) UNIQUE,
+    PRIMARY KEY(id)
+);
+
+DROP TABLE tracktoartist CASCADE;
+CREATE TABLE tracktoartist (
+    id SERIAL,
+    track VARCHAR(128),
+    track_id INTEGER REFERENCES track(id) ON DELETE CASCADE,
+    artist VARCHAR(128),
+    artist_id INTEGER REFERENCES artist(id) ON DELETE CASCADE,
+    PRIMARY KEY(id)
+);
+
+\copy track(title,artist,album,count,rating,len) FROM 'library.csv' WITH DELIMITER ',' CSV;
+
+INSERT INTO album (title) SELECT DISTINCT album FROM track;
+UPDATE track SET album_id = (SELECT album.id FROM album WHERE album.title = track.album);
+
+INSERT INTO tracktoartist (track, artist) SELECT DISTINCT ...
+
+INSERT INTO artist (name) ...
+
+UPDATE tracktoartist SET track_id = ...
+UPDATE tracktoartist SET artist_id = ...
+
+-- We are now done with these text fields
+ALTER TABLE track DROP COLUMN album;
+ALTER TABLE track ...
+ALTER TABLE tracktoartist DROP COLUMN track;
+ALTER TABLE tracktoartist ...
+
+SELECT track.title, album.title, artist.name
+FROM track
+JOIN album ON track.album_id = album.id
+JOIN tracktoartist ON track.id = tracktoartist.track_id
+JOIN artist ON tracktoartist.artist_id = artist.id
+LIMIT 3;
+</pre>
