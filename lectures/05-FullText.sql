64566a99f52855510fa076fd3322d1326bb60414
Charles Severance <csev@umich.edu>
2019-11-12 17:29:58 -0500

    Updates

--- lectures/05-FullText.sql
+++ lectures/05-FullText.sql
@@ -1,4 +1,8 @@
 
+-- Strings, arrays, and rows
+
+SELECT string_to_array('Hello world', ' ');
+SELECT unnest(string_to_array('Hello world', ' '));
 
 CREATE TABLE docs (id SERIAL, doc TEXT, PRIMARY KEY(id));
 INSERT INTO docs (doc) VALUES
@@ -9,10 +13,12 @@ SELECT * FROM docs;
 
 --- https://stackoverflow.com/questions/29419993/split-column-into-multiple-rows-in-postgres
 
+-- Break the document column into one row per word + primary key
 SELECT id, s.keyword AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 ORDER BY id;
 
+-- Discard duplicate rows
 SELECT DISTINCT id, s.keyword AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 ORDER BY id;
@@ -22,6 +28,7 @@ CREATE TABLE docs_gin (
   doc_id INTEGER REFERENCES docs(id) ON DELETE CASCADE
 );
 
+-- Insert the keyword / primary key rows into a table
 INSERT INTO docs_gin (doc_id, keyword)
 SELECT DISTINCT id, s.keyword AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
@@ -29,42 +36,59 @@ ORDER BY id;
 
 SELECT * FROM docs_gin ORDER BY doc_id;
 
-SELECT doc FROM docs AS D
+-- Find all the distinct documents that match a keyword
+SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
 WHERE G.keyword = 'UMSI';
 
-SELECT doc FROM docs AS D
-JOIN docs_gin AS G ON D.id = G.doc_id
-WHERE G.keyword in ('SQL', 'Python');
-
+-- We can remove duplicates and have more than one keyword
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
-WHERE G.keyword in ('SQL', 'Python');
+WHERE G.keyword IN ('fun', 'people');
 
--- docs_gin is a string (not language) based Inverted Index
+-- We can handle a phrase
+SELECT DISTINCT doc FROM docs AS D
+JOIN docs_gin AS G ON D.id = G.doc_id
+WHERE G.keyword = ANY(string_to_array('I want to learn', ' '));
+
+-- This can go sideways - (foreshadowing stop words)
+SELECT DISTINCT doc FROM docs AS D
+JOIN docs_gin AS G ON D.id = G.doc_id
+WHERE G.keyword = ANY(string_to_array('Search for Lemons and Neons', ' '));
+
+-- docs_gin is purely a text (not language) based Inverted Index
 -- PostgreSQL already knows how to do this using the GIN index
 
 DROP TABLE docs cascade;
 DROP INDEX gin1;
 CREATE TABLE docs (id SERIAL, doc TEXT, PRIMARY KEY(id));
+
+-- The GIN (General Inverted Index) thinks about columns that contain arrays
+-- A GIN needs to know what kind of data will be in the arrays 
+-- _text_ops means that it is expecting text[] (arrays of strings)
+-- and WHERE clauses will use array operators (i.e. like <@ )
+
 CREATE INDEX gin1 ON docs USING gin(string_to_array(doc, ' ')  _text_ops);
+
 INSERT INTO docs (doc) VALUES
 ('This is SQL and Python and other fun teaching stuff'),
 ('More people should learn SQL from UMSI'),
 ('UMSI also teaches Python and also SQL');
 
-SELECT doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
-EXPLAIN SELECT doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
+-- The <@ if "is contained within" or "intersection" from set theory
+SELECT id, doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
+EXPLAIN SELECT id, doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
 
-
--- If we know the documents are language, we can create smaller indexes
+-- If we know the documents contain natural language, we can optimize indexes
 
 -- (1) Ignore the case of words in the index and in the query
--- (2) Don't index low-meaning "stop words" that we won't search for
+-- (2) Don't index low-meaning "stop words" that we will ignore
+-- if they are in a search query 
 
 CREATE TABLE stop_words (word TEXT unique);
 INSERT INTO stop_words (word) VALUES ('is'), ('this'), ('and');
 
+-- All we do is throw out the words in the stop word list
 SELECT DISTINCT id, lower(s.keyword) AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 WHERE s.keyword NOT IN (SELECT word FROM stop_words)
@@ -72,6 +96,7 @@ ORDER BY id;
 
 DELETE FROM docs_gin;
 
+-- Put the stop-word free list into the GIN
 INSERT INTO docs_gin (doc_id, keyword)
 SELECT DISTINCT id, lower(s.keyword) AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
@@ -80,38 +105,45 @@ ORDER BY id;
 
 SELECT * FROM docs_gin;
 
+-- A one word query
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
 WHERE G.keyword = lower('UMSI');
 
+-- A multi-word query
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
-WHERE G.keyword in (lower('fun'), lower('people'));
+WHERE G.keyword = 
+  ANY(string_to_array(lower('Meet fun people'), ' '));
 
-SELECT DISTINCT id, doc FROM docs AS D
+-- A stop word query - as if it were never there
+SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
-WHERE G.keyword in (lower('SQL'), lower('Python'));
+WHERE G.keyword = lower('and');
 
 -- We can make the index even smaller
 --- (3) Only store the "stems" of words
+
+-- Our simple approach is to make a "dictionary" of word -> stem
 
 CREATE TABLE docs_stem (word TEXT, stem TEXT);
 INSERT INTO docs_stem (word, stem) VALUES
 ('teaching', 'teach'), ('teaches', 'teach');
 
--- unnest() expands an array column into multiple rows with one value
--- https://www.postgresql.org/docs/10/functions-array.html
+-- Move the initial word extraction into a sub-query
 SELECT id, keyword FROM (
 SELECT DISTINCT id, lower(s.keyword) AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 ) AS X;
 
+-- Add the stems as third column (may or may not exist)
 SELECT id, keyword, stem FROM (
 SELECT DISTINCT id, lower(s.keyword) AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 ) AS K
 LEFT JOIN docs_stem AS S ON K.keyword = S.word;
 
+-- If the stem is there, use it
 SELECT id,
 CASE WHEN stem IS NOT NULL THEN stem ELSE keyword END,
 keyword, stem
@@ -121,14 +153,41 @@ FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
 ) AS K
 LEFT JOIN docs_stem AS S ON K.keyword = S.word;
 
-DELETE FROM docs_gin;
+-- Null Coalescing - return the first non-null in a list
+-- x = stem or 'teaching'  # Python
+SELECT COALESCE(NULL, NULL, 'umsi');
+SELECT COALESCE('umsi', NULL, 'SQL');
 
-INSERT INTO docs_gin (doc_id, keyword)
-SELECT id,
-CASE WHEN stem IS NOT NULL THEN stem ELSE keyword END
+-- If the stem is there, use it instead of the keyword
+SELECT id, COALESCE(stem, keyword)
 FROM (
 SELECT DISTINCT id, lower(s.keyword) AS keyword
 FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
+) AS K
+LEFT JOIN docs_stem AS S ON K.keyword = S.word;
+
+-- Insert only the stems
+DELETE FROM docs_gin;
+
+INSERT INTO docs_gin (doc_id, keyword)
+SELECT id, COALESCE(stem, keyword)
+FROM (
+  SELECT DISTINCT id, lower(s.keyword) AS keyword
+  FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
+) AS K
+LEFT JOIN docs_stem AS S ON K.keyword = S.word;
+
+SELECT * FROM docs_gin;
+
+-- Lets do stop words and stems...
+DELETE FROM docs_gin;
+
+INSERT INTO docs_gin (doc_id, keyword)
+SELECT id, COALESCE(stem, keyword)
+FROM (
+  SELECT DISTINCT id, lower(s.keyword) AS keyword
+  FROM docs AS D, unnest(string_to_array(D.doc, ' ')) s(keyword)
+  WHERE s.keyword NOT IN (SELECT word FROM stop_words)
 ) AS K
 LEFT JOIN docs_stem AS S ON K.keyword = S.word;
 
@@ -136,27 +195,69 @@ SELECT * FROM docs_gin;
 
 -- Like Python null/false coalescing
 -- x = stem or 'teaching'
-SELECT COALESCE((SELECT stem FROM docs_stem WHERE word=lower('teaching')), lower('teaching'));
 SELECT COALESCE((SELECT stem FROM docs_stem WHERE word=lower('SQL')), lower('SQL'));
 
+-- Handling the stems in queries.  Use the keyword if there is no stem
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
 WHERE G.keyword = COALESCE((SELECT stem FROM docs_stem WHERE word=lower('SQL')), lower('SQL'));
 
+-- Prefer the stem over the actual keyword
+SELECT COALESCE((SELECT stem FROM docs_stem WHERE word=lower('teaching')), lower('teaching'));
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
 WHERE G.keyword = COALESCE((SELECT stem FROM docs_stem WHERE word=lower('teaching')), lower('teaching'));
 
--- There is an easier way :)
+-- The technical term for converting search terms to their stems is called "conflation"
+-- from https://en.wikipedia.org/wiki/Stemming
 
+-- Of course PostgreSQL makes this all so much easier and more efficient :)
+
+-- ts_vector is an special "array" of stemmed words and a stop-word 
+-- filter + positions within the document
 SELECT to_tsvector('english', 'This is SQL and Python and other fun teaching stuff');
 SELECT to_tsvector('english', 'More people should learn SQL from UMSI');
 SELECT to_tsvector('english', 'UMSI also teaches Python and also SQL');
 
+-- ts_query is an "array" of lower case, pre-stemmed words with 
+-- stop words removed plus logical operators & = and, ! = not, | = or
 SELECT to_tsquery('english', 'teaching');
 SELECT to_tsquery('english', 'teaches');
-SELECT to_tsquery('english', 'Teach');
+SELECT to_tsquery('english', 'and');
 SELECT to_tsquery('english', 'SQL');
+SELECT to_tsquery('english', 'Teach | teaches | teaching | and | the | if');
+
+-- Plaintext just pulls out the keywords
+SELECT plainto_tsquery('english', 'SQL Python');
+SELECT plainto_tsquery('english', 'Teach teaches teaching and the if');
+
+-- A phrase is words that come in order
+SELECT phraseto_tsquery('english', 'SQL Python');
+
+-- Websearch is in PostgreSQL >= 11 and a bit like 
+-- https://www.google.com/advanced_search
+SELECT websearch_to_tsquery('english', 'SQL -not Python');
+
+SELECT to_tsquery('english', 'teaching') @@ 
+  to_tsvector('english', 'UMSI also teaches Python and also SQL');
+
+-- Lets do a naturla language inverted indes letting PostgreSQL do the work
+
+DROP TABLE docs cascade;
+DROP INDEX gin1;
+
+CREATE TABLE docs (id SERIAL, doc TEXT, PRIMARY KEY(id));
+CREATE INDEX gin1 ON docs USING gin(to_tsvector('english', doc));
+
+INSERT INTO docs (doc) VALUES
+('This is SQL and Python and other fun teaching stuff'),
+('More people should learn SQL from UMSI'),
+('UMSI also teaches Python and also SQL');
+
+SELECT id, doc FROM docs WHERE 
+    to_tsquery('english', 'learn') @@ to_tsvector('english', doc);
+EXPLAIN SELECT id, doc FROM docs WHERE 
+    to_tsquery('english', 'learn') @@ to_tsvector('english', doc);
 
 -- wget https://www.pg4e.com/code/gmane.py
 -- python3 gmane.py
@@ -178,7 +279,7 @@ SELECT to_tsquery('english', 'neon') @@ to_tsvector('english', body) FROM messag
 
 SELECT to_tsquery('english', 'monday') @@ to_tsvector('english', body) FROM messages LIMIT 1;
 
--- https://www.postgresql.org/docs/10/textsearch-indexes.html
+-- https://www.postgresql.org/docs/11/textsearch-indexes.html
 CREATE INDEX messages_gin ON messages USING gin(to_tsvector('english', body));
 
 SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
@@ -191,7 +292,7 @@ DROP INDEX messages_gist;
 SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
 EXPLAIN ANALYZE SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
 
--- https://www.postgresql.org/docs/10/functions-textsearch.html
+-- https://www.postgresql.org/docs/11/functions-textsearch.html
 SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'personal & learning') @@ to_tsvector('english', body);
 SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'learning & personal') @@ to_tsvector('english', body);
 SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'personal <-> learning') @@ to_tsvector('english', body);
@@ -220,11 +321,12 @@ FROM messages
 WHERE to_tsquery('english', 'personal & learning') @@ to_tsvector('english', body)
 ORDER BY ts_rank DESC;
 
--- Wait for PostgreSQL 11
 SELECT id, subject, sender FROM messages WHERE to_tsquery('english', '! personal & learning') @@ to_tsvector('english', body);
+
+-- websearch_to_tsquery is in PostgreSQL > 11
 SELECT id, subject, sender FROM messages WHERE websearch_to_tsquery('english', '-personal learning') @@ to_tsvector('english', body);
 
--- https://www.postgresql.org/docs/10/textsearch-indexes.html
+-- https://www.postgresql.org/docs/11/textsearch-indexes.html
 
 --- GIN versus GIsT
 
