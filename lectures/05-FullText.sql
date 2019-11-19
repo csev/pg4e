cacd3227d16e1e4d5693c0c58c8dbfcfe1db8ddc
Charles Severance <csev@umich.edu>
2019-11-19 17:11:33 -0500

    Updates

--- lectures/05-FullText.sql
+++ lectures/05-FullText.sql
@@ -1,8 +1,12 @@
 
+-- https://www.pg4e.com/lectures/05-FullText.sql
+
 -- Strings, arrays, and rows
 
 SELECT string_to_array('Hello world', ' ');
 SELECT unnest(string_to_array('Hello world', ' '));
+
+-- Inverted string index with SQL
 
 CREATE TABLE docs (id SERIAL, doc TEXT, PRIMARY KEY(id));
 INSERT INTO docs (doc) VALUES
@@ -64,7 +68,7 @@ DROP INDEX gin1;
 CREATE TABLE docs (id SERIAL, doc TEXT, PRIMARY KEY(id));
 
 -- The GIN (General Inverted Index) thinks about columns that contain arrays
--- A GIN needs to know what kind of data will be in the arrays 
+-- A GIN needs to know what kind of data will be in the arrays
 -- array_ops (_text_ops for PostgreSQL 9) means that it is expecting
 -- text[] (arrays of strings) and WHERE clauses will use array
 -- operators (i.e. like <@ )
@@ -84,11 +88,14 @@ INSERT INTO docs (doc) VALUES
 SELECT id, doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
 EXPLAIN SELECT id, doc FROM docs WHERE '{learn}' <@ string_to_array(doc, ' ');
 
+
+-- Inverted string index wht stop words using SQL
+
 -- If we know the documents contain natural language, we can optimize indexes
 
 -- (1) Ignore the case of words in the index and in the query
 -- (2) Don't index low-meaning "stop words" that we will ignore
--- if they are in a search query 
+-- if they are in a search query
 
 CREATE TABLE stop_words (word TEXT unique);
 INSERT INTO stop_words (word) VALUES ('is'), ('this'), ('and');
@@ -118,7 +125,7 @@ WHERE G.keyword = lower('UMSI');
 -- A multi-word query
 SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
-WHERE G.keyword = 
+WHERE G.keyword =
   ANY(string_to_array(lower('Meet fun people'), ' '));
 
 -- A stop word query - as if it were never there
@@ -126,8 +133,10 @@ SELECT DISTINCT doc FROM docs AS D
 JOIN docs_gin AS G ON D.id = G.doc_id
 WHERE G.keyword = lower('and');
 
+-- Add stemming
+
 -- We can make the index even smaller
---- (3) Only store the "stems" of words
+-- (3) Only store the "stems" of words
 
 -- Our simple approach is to make a "dictionary" of word -> stem
 
@@ -216,15 +225,15 @@ WHERE G.keyword = COALESCE((SELECT stem FROM docs_stem WHERE word=lower('teachin
 -- The technical term for converting search terms to their stems is called "conflation"
 -- from https://en.wikipedia.org/wiki/Stemming
 
--- Of course PostgreSQL makes this all so much easier and more efficient :)
+-- Using PostgreSQL built-in features (much easier and more efficient)
 
--- ts_vector is an special "array" of stemmed words and a stop-word 
+-- ts_vector is an special "array" of stemmed words, passed throug a stop-word
 -- filter + positions within the document
 SELECT to_tsvector('english', 'This is SQL and Python and other fun teaching stuff');
 SELECT to_tsvector('english', 'More people should learn SQL from UMSI');
 SELECT to_tsvector('english', 'UMSI also teaches Python and also SQL');
 
--- ts_query is an "array" of lower case, pre-stemmed words with 
+-- ts_query is an "array" of lower case, stemmed words with
 -- stop words removed plus logical operators & = and, ! = not, | = or
 SELECT to_tsquery('english', 'teaching');
 SELECT to_tsquery('english', 'teaches');
@@ -239,11 +248,11 @@ SELECT plainto_tsquery('english', 'Teach teaches teaching and the if');
 -- A phrase is words that come in order
 SELECT phraseto_tsquery('english', 'SQL Python');
 
--- Websearch is in PostgreSQL >= 11 and a bit like 
+-- Websearch is in PostgreSQL >= 11 and a bit like
 -- https://www.google.com/advanced_search
 SELECT websearch_to_tsquery('english', 'SQL -not Python');
 
-SELECT to_tsquery('english', 'teaching') @@ 
+SELECT to_tsquery('english', 'teaching') @@
   to_tsvector('english', 'UMSI also teaches Python and also SQL');
 
 -- Lets do a natural language inverted index letting PostgreSQL do the work
@@ -259,10 +268,12 @@ INSERT INTO docs (doc) VALUES
 ('More people should learn SQL from UMSI'),
 ('UMSI also teaches Python and also SQL');
 
-SELECT id, doc FROM docs WHERE 
+SELECT id, doc FROM docs WHERE
     to_tsquery('english', 'learn') @@ to_tsvector('english', doc);
-EXPLAIN SELECT id, doc FROM docs WHERE 
+EXPLAIN SELECT id, doc FROM docs WHERE
     to_tsquery('english', 'learn') @@ to_tsvector('english', doc);
+
+-- Using natural language on an email corpus
 
 -- wget https://www.pg4e.com/code/gmane.py
 -- python3 gmane.py
@@ -280,37 +291,58 @@ SELECT to_tsvector('english', body) FROM messages LIMIT 1;
 
 SELECT to_tsquery('english', 'monday');
 
-SELECT to_tsquery('english', 'neon') @@ to_tsvector('english', body) FROM messages LIMIT 1;
+SELECT to_tsquery('english', 'neon') @@ to_tsvector('english', body)
+FROM messages LIMIT 1;
 
-SELECT to_tsquery('english', 'monday') @@ to_tsvector('english', body) FROM messages LIMIT 1;
+SELECT to_tsquery('english', 'monday') @@ to_tsvector('english', body)
+FROM messages LIMIT 1;
 
 -- https://www.postgresql.org/docs/11/textsearch-indexes.html
 CREATE INDEX messages_gin ON messages USING gin(to_tsvector('english', body));
 
-SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
-EXPLAIN ANALYZE SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
+SELECT subject, sender FROM messages
+WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
+EXPLAIN ANALYZE SELECT subject, sender FROM messages
+WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
 
 DROP INDEX messages_gin;
 CREATE INDEX messages_gist ON messages USING gist(to_tsvector('english', body));
 DROP INDEX messages_gist;
 
-SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
-EXPLAIN ANALYZE SELECT subject, sender FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
+SELECT subject, sender FROM messages
+WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
+EXPLAIN ANALYZE SELECT subject, sender
+FROM messages WHERE to_tsquery('english', 'monday') @@ to_tsvector('english', body);
 
 -- https://www.postgresql.org/docs/11/functions-textsearch.html
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'personal & learning') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'learning & personal') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'personal <-> learning') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'learning <-> personal') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', '! personal & learning') @@ to_tsvector('english', body);
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', 'personal & learning') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', 'learning & personal') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', 'personal <-> learning') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', 'learning <-> personal') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', '! personal & learning') @@ to_tsvector('english', body);
 
 -- plainto_tsquery() Is tolerant of "syntax errors" in the expression
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', '(personal learning') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE plainto_tsquery('english', '(personal learning') @@ to_tsvector('english', body);
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', '(personal learning') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE plainto_tsquery('english', '(personal learning') @@ to_tsvector('english', body);
 
 -- phraseto_tsquery() implies followed by
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', 'I <-> think') @@ to_tsvector('english', body);
-SELECT id, subject, sender FROM messages WHERE phraseto_tsquery('english', 'I think') @@ to_tsvector('english', body);
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', 'I <-> think') @@ to_tsvector('english', body);
+
+SELECT id, subject, sender FROM messages
+WHERE phraseto_tsquery('english', 'I think') @@ to_tsvector('english', body);
 
 -- https://www.postgresql.org/docs/12/textsearch-controls.html#TEXTSEARCH-RANKING
 SELECT id, subject, sender,
@@ -326,10 +358,12 @@ FROM messages
 WHERE to_tsquery('english', 'personal & learning') @@ to_tsvector('english', body)
 ORDER BY ts_rank DESC;
 
-SELECT id, subject, sender FROM messages WHERE to_tsquery('english', '! personal & learning') @@ to_tsvector('english', body);
+SELECT id, subject, sender FROM messages
+WHERE to_tsquery('english', '! personal & learning') @@ to_tsvector('english', body);
 
 -- websearch_to_tsquery is in PostgreSQL > 11
-SELECT id, subject, sender FROM messages WHERE websearch_to_tsquery('english', '-personal learning') @@ to_tsvector('english', body);
+SELECT id, subject, sender FROM messages
+WHERE websearch_to_tsquery('english', '-personal learning') @@ to_tsvector('english', body);
 
 -- https://www.postgresql.org/docs/11/textsearch-indexes.html
 
