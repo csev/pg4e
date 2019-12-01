a862794ad72d68c8c083139ffaab73275714e80a
Charles Severance <csev@umich.edu>
2019-12-01 02:55:51 -0500

    Last updates for tonight.

--- lectures/06-JSON.sql
+++ lectures/06-JSON.sql
@@ -41,47 +41,68 @@ SELECT url, status FROM SWAPI where URL like '%film%';
 SELECT COUNT(url) FROM swapi;
 
 SELECT url FROM swapi WHERE status != 200;
+-- Load complete...
 
-SELECT url FROM swapi WHERE body @> '{"director": "George Lucas"}';
-EXPLAIN SELECT url FROM swapi WHERE body @> '{"director": "George Lucas"}';
-
-CREATE INDEX swapi_gin ON swapi USING gin (body);
-EXPLAIN SELECT url FROM swapi WHERE body @> '{"director": "George Lucas"}';
-
--- oops - Not what we meant
-SELECT url FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb);
-
--- We can fix that...
-SELECT url FROM swapi WHERE body->>'url' LIKE 'https://swapi.co/api/films/%';
-EXPLAIN SELECT url FROM swapi WHERE body->>'url' LIKE 'https://swapi.co/api/films/%';
-
--- But with no index...
-SELECT url FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb) AND body->>'url' LIKE 'https://swapi.co/api/films/%';
-
-
--- https://stackoverflow.com/questions/13615760/add-element-to-json-object-in-postgres
-
-SELECT body->'url' FROM swapi LIMIT 1;
-SELECT (body->'url')::text FROM swapi LIMIT 1;
+-- The "->>" implies get the attribute and convert to a string
+SELECT body->>'url' FROM swapi LIMIT 1;
 
 -- Parenthesis matter - cast has higher precedence than ->
-SELECT pg_typeof(body->'url'::text) FROM swapi LIMIT 1;
+SELECT pg_typeof(body->'url') FROM swapi LIMIT 1;
+SELECT pg_typeof(body->'url'::text) FROM swapi LIMIT 1;  -- is jsonb, not text
 SELECT pg_typeof((body->'url')::text) FROM swapi LIMIT 1;
+SELECT pg_typeof(body->>'url') FROM swapi LIMIT 1; -- text and succinct
 
-SELECT substring((body->'url')::text, 'https://swapi.co/api/([a-z]+)/') FROM swapi LIMIT 1;
+SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
+EXPLAIN SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
 
-SELECT ('{"type": "' || substring((body->'url')::text, 'https://swapi.co/api/([a-z]+)/') || '" }')
+CREATE INDEX swapi_gin ON swapi USING gin (body);
+EXPLAIN SELECT body->>'url' FROM swapi WHERE body @> '{"director": "George Lucas"}';
+
+-- oops - Not what we meant
+SELECT body->>'url' FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb);
+
+-- We can fix that...
+SELECT body->>'url' FROM swapi WHERE body->>'url' LIKE 'https://swapi.co/api/films/%';
+EXPLAIN SELECT body->>'url' FROM swapi WHERE body->>'url' LIKE 'https://swapi.co/api/films/%';
+
+-- We can do a sequential scan
+SELECT body->>'url' FROM swapi WHERE NOT(body @> '{"director": "George Lucas"}'::jsonb) AND 
+body->>'url' LIKE 'https://swapi.co/api/films/%';
+
+SELECT body->>'url', body->>'name' FROM swapi LIMIT 10;
+
+SELECT body->>'url', body->>'name' FROM swapi 
+WHERE body->>'url' LIKE 'https://swapi.co/api/people/%' LIMIT 10;
+
+SELECT body->>'url', body->>'name' FROM swapi 
+WHERE body->>'url' LIKE 'https://swapi.co/api/species/%' LIMIT 10;
+
+-- LIKE makes a sequential scan
+EXPLAIN SELECT body->>'url', body->>'name' FROM swapi 
+WHERE body->>'url' LIKE 'https://swapi.co/api/species/%' LIMIT 10;
+
+-- Lets augment the JSON
+-- https://stackoverflow.com/questions/13615760/add-element-to-json-object-in-postgres
+
+SELECT substring(body->>'url', 'https://swapi.co/api/([a-z]+)/') FROM swapi LIMIT 1;
+
+SELECT ('{"type": "' || substring(body->>'url', 'https://swapi.co/api/([a-z]+)/') || '" }')
 FROM swapi LIMIT 1;
-SELECT ('{"type": "' || substring((body->'url')::text, 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb
+SELECT ('{"type": "' || substring(body->>'url', 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb
 FROM swapi LIMIT 1;
-SELECT body || ('{"type": "' || substring((body->'url')::text, 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb
+
+-- Merge new json back into the body
+SELECT body || ('{"type": "' || substring(body->>'url', 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb
 FROM swapi LIMIT 1;
 
 -- Add the type field to all the records
-UPDATE swapi SWT SET body = body || ('{"type": "' || substring((body->'url')::text, 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb;
+UPDATE swapi SWT SET body = body || ('{"type": "' || substring(body->>'url', 'https://swapi.co/api/([a-z]+)/') || '" }')::jsonb;
 
+SELECT body->>'url', body->>'name' FROM swapi WHERE body @> '{"type": "species"}'  LIMIT 10;
+EXPLAIN SELECT body->>'url', body->>'name' FROM swapi WHERE body @> '{"type": "species"}'  LIMIT 10;
+
+-- Dang this does not use the GIN (after all that!)
 SELECT url FROM swapi WHERE body @> '{"director": "George Lucas", "type": "films"}';
-SELECT url FROM swapi WHERE body @> '{"type": "films"}' AND NOT(body @> '{"director": "George Lucas"}');
+EXPLAIN SELECT url FROM swapi WHERE body @> '{"director": "George Lucas", "type": "films"}';
 
-EXLPAIN SELECT url FROM swapi WHERE body @> '{"type": "films"}' AND NOT(body @> '{"director": "George Lucas"}');
 
