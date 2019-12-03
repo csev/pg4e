22cb9420fa1188e6f7a9298b7bfef3e79688038e
Charles Severance <csev@umich.edu>
2019-12-03 14:05:48 -0500

    Updates

--- code/hidden-dist.py
+++ code/hidden-dist.py
@@ -2,7 +2,7 @@
 # https://www.pg4e.com/code/hidden-dist.py
 
 def secrets():
-    return {"host": "35.123.23.37",
-            "user": "pg4e_user_42",
-            "pass": "pg4e_pass_42",
-            "database": "pg4e"}
+    return {'host': '35.123.23.37',
+            'user': 'pg4e_user_42',
+            'pass': 'pg4e_pass_42',
+            'database': 'pg4e'}
---
+++ code/simple.py
@@ -0,0 +1,54 @@
+import psycopg2
+import hidden
+
+# Load the secrets
+secrets = hidden.secrets()
+
+conn = psycopg2.connect(host=secrets['host'],
+        database=secrets['database'], 
+        user=secrets['user'], 
+        password=secrets['pass'], 
+        connect_timeout=3)
+
+cur = conn.cursor()
+
+sql = 'DROP TABLE IF EXISTS pythonfun CASCADE;'
+print(sql)
+cur.execute(sql)
+
+sql = 'CREATE TABLE pythonfun (id SERIAL, line TEXT);'
+print(sql)
+cur.execute(sql)
+
+conn.commit()    # Flush it all to the DB server
+
+for i in range(10) : 
+    txt = "Have a nice day "+str(i)
+    sql = 'INSERT INTO pythonfun (line) VALUES (%s);'
+    cur.execute(sql, (txt, ))
+
+conn.commit()
+
+sql = "SELECT id, line FROM pythonfun WHERE id=5;" 
+print(sql)
+cur.execute(sql)
+
+row = cur.fetchone()
+if row is None : 
+    print('Row not found')
+else:
+    print('Found', row)
+
+sql = 'INSERT INTO pythonfun (line) VALUES (%s) RETURNING id;'
+cur.execute(sql, (txt, ))
+id = cur.fetchone()[0]
+print('New id', id)
+
+# Lets make a mistake
+sql = "SELECT line FROM pythonfun WHERE mistake=5;"
+print(sql)
+cur.execute(sql)
+
+conn.commit()
+cur.close()
+
