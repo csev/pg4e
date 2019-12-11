df152d04c54765a709994e47df076b4454351135
Charles Severance <csev@umich.edu>
2019-12-11 13:45:32 +0900

    Nice search added

--- code/elastic0.py
+++ code/elastic0.py
@@ -1,3 +1,5 @@
+
+# 
 from datetime import datetime
 from elasticsearch import Elasticsearch
 
@@ -13,20 +15,64 @@ es = Elasticsearch(
     scheme="http",
 )
 
+# Start fresh
+# https://elasticsearch-py.readthedocs.io/en/master/api.html#indices
+
+res = es.indices.delete(index='test-index', ignore=[400, 404])
+print("Dropped index")
+print(res)
+
+settings = {
+        "mappings": {
+            "tweet": {
+                "properties": {
+                    "author": {
+                        "type": "keyword"
+                    },
+                    "text": {
+                        "type": "text"
+                    },
+                    "timestamp": {
+                        "type": "date"
+                    },
+                }
+            }
+        }
+    }
+
+res = es.indices.create(index='test-index', body=settings)
+print("Created the index...")
+print(res)
+
 doc = {
     'author': 'kimchy',
     'text': 'Elasticsearch: cool. bonsai cool.',
     'timestamp': datetime.now(),
 }
 
-# res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-# print(res['result'])
+# Note - you can't change the key type after you start indexing documents
+# res = es.index(index="test-index", id='abc', body=doc)
+res = es.index(index="test-index", doc_type='tweet', id='abc', body=doc)
+print('Added document...')
+print(res['result'])
 
-res = es.get(index="test-index", doc_type='tweet', id=1)
+res = es.get(index="test-index", doc_type='tweet', id='abc')
+print('Retrieved document...')
+print(res)
 
-es.indices.refresh(index="test-index")
+# Tell it to recompute the index
+res = es.indices.refresh(index="test-index")
+print("Index refreshed")
+print(res)
 
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
+# Do a search
+# res = es.search(index="test-index", body={"query": {"match_all": {}}})
+
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-match-query.html
+
+res = es.search(index="test-index", body={"query": {"match": { "text" : { "query": "bonsai" }}}})
+print('Search results...')
+print(res)
 print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
     s = hit['_source']
