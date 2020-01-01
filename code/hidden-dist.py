6c722cd7b84a6d333e1ca9328c2c64d921f0686d
Charles Severance <csev@umich.edu>
2020-01-01 14:16:32 -0500

    Updates

--- code/hidden-dist.py
+++ code/hidden-dist.py
@@ -2,7 +2,7 @@
 
 def secrets():
     return {"host": "35.222.5.70",
-            "port": 1009,
+            "port": 13009,
             "user": "pg4e_user_8087f",
             "pass": "pg4e_pass_7a749",
             "database": "pg4e"}
@@ -12,4 +12,10 @@ def elastic():
             "port": 13009,
             "user": "admin",
             "pass": "sakaiger"}
+
+def readonly():
+    return {"host": "35.239.113.162",
+            "port": 13009,
+            "user": "pg4e_data_read",
+            "pass": "tsugi"}
 
