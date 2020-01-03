d5b064f18ce4a8b0461353429d47de402b01711b
Charles Severance <csev@umich.edu>
2020-01-03 08:22:53 -0500

    Progress

--- code/hidden-dist.py
+++ code/hidden-dist.py
@@ -1,10 +1,17 @@
 # Keep this file separate
 
+# psql -h 35.239.113.162 -p 13009 -U pg4e_user_8187f pg4e
+
+# %load_ext sql
+# %config SqlMagic.autocommit=False
+# %sql postgresql://pg4e_user_8187f:pg4e_pass_7f749@35.222.5.70:13009/pg4e
+# %sql SELECT 1 as "Test"
+
 def secrets():
     return {"host": "35.222.5.70",
             "port": 13009,
-            "user": "pg4e_user_8087f",
-            "pass": "pg4e_pass_7a749",
+            "user": "pg4e_user_8187f",
+            "pass": "pg4e_pass_7f749",
             "database": "pg4e"}
 
 def elastic():
