a7aa8fb139847f9013469582f39b42203b39fe33
Charles Severance <csev@umich.edu>
2020-02-03 15:00:34 -0500

    Progress.

--- tools/sql/config.php
+++ tools/sql/config.php
@@ -2,6 +2,7 @@
 require_once "../config.php";
 require_once "sql_util.php";
 
+use \Tsugi\Util\U;
 use \Tsugi\Core\Settings;
 use \Tsugi\Core\LTIX;
 use \Tsugi\UI\SettingsForm;
@@ -13,11 +14,22 @@ $unique = getUnique($LAUNCH);
 
 if ( ! $USER->instructor ) die("Must be instructor");
 
-$oldsettings = Settings::linkGetAll();
+$redirect = false;
+$postkeys = array('db_source', 'psql_url', 'psql_secret', 'psql_key', 'tunnel');
+foreach($postkeys as $key) {
+    if ( U::get($_POST, $key) ) {
+        $LAUNCH->context->settingsSet($key, U::get($_POST, $key));
+        $redirect = true;
+    }
+}
 
-$assn = Settings::linkGet('exercise');
+if ( $redirect ) {
+    $_SESSION['success'] = 'Settings updated.';
+    header("Location: ".addSession('index.php'));
+    return;
+}
 
-$tunnel = $LAUNCH->link->settingsGet('tunnel');
+$settings = $LAUNCH->context->settingsGetAll();
 
 // View
 $OUTPUT->header();
@@ -34,7 +46,40 @@ $OUTPUT->flashMessages();
 
 $OUTPUT->welcomeUserCourse();
 
-echo("<h1>Yada</h1>\n");
+// echo("<pre>\n");var_dump($set);echo("</pre>\n");
+
+?>
+<p>Please configure the database source(s) for this course.</p>
+<form method="post">
+<p>
+<select name="db_source">
+<option value="none">-- Please select the type of database server --</option>
+<option value="umsi"
+<?php if ( U::get($settings, "db_source") == 'umsi' ) echo('selected'); ?>
+>UMSI</option>
+<option value="elephant"
+<?php if ( U::get($settings, "db_source") == 'elephant' ) echo('selected'); ?>
+>ElephantSQL</option>
+</select>
+</p>
+<p>If you are using ElephantSQL, please leave the PSQL provisioning values blank.</p>
+<p>PSQL_URL <input type="text" name="psql_url" value="<?= htmlentities(U::get($settings, 'psql_url')) ?>"></p>
+<p>PSQL_KEY <input type="text" name="psql_key" value="<?= htmlentities(U::get($settings, 'psql_key')) ?>"></p>
+<p>PSQL_SECRET <input type="text" name="psql_secret" value="<?= htmlentities(U::get($settings, 'psql_secret')) ?>"></p>
+<p>
+Is there an ssh tunnel required?
+<select name="tunnel">
+<option value="no">No Tunnel<option>
+<option value="yes"
+<?php if ( U::get($settings, "tunnel") == 'yes' ) echo('selected'); ?>
+>SSH Tunnel</option>
+</select>
+</p>
+<input type="submit">
+</form>
+
+<?php
+
 
 $OUTPUT->footer();
 
