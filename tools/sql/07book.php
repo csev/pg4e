6485616af3228c2b3a87844aacc9626b12b226d6
Charles Severance <csev@umich.edu>
2020-02-24 17:51:56 -0500

    Wipe out cookies when switching versions.

--- tools/sql/07book.php
+++ tools/sql/07book.php
@@ -1,6 +1,20 @@
 <?php
 
+$get_version = $_GET['es_version'] ?? false;
+$cookie_version = $_COOKIE['es_version'] ?? false;
+
 $the_version = $_GET['es_version'] ?? $_COOKIE['es_version'] ?? 'elastic6';
+
+// When we are switching - wipe out the cookies
+if ( strlen($get_version) > 0 && $get_version != $cookie_version ) {
+    $vars = array("es_host", "es_prefix", "es_port", "es_user", "es_pass");
+    foreach($vars as $var) {
+        setcookie($var, '', time()+31556926 ,'/');
+        unset($_SESSION[$var]);
+        unset($_COOKIE[$var]);
+    }
+    $the_version = $_GET['es_version'] ?? 'elastic6';
+}
 
 setcookie("es_version", $the_version, time()+31556926 ,'/');
 if ( $the_version == 'elastic7' ) {
