06a05461e67e45702d0b53b81e5372fbf267b164
Charles Severance <csev@umich.edu>
2020-04-12 15:26:14 -0400

    Updates

--- scripts/docker/tsugi-pg4e-startup.sh
+++ scripts/docker/tsugi-pg4e-startup.sh
@@ -5,6 +5,22 @@ bash /usr/local/bin/tsugi-dev-startup.sh return
 echo Starting PostgreSQL
 service postgresql start
 
+# https://stackoverflow.com/questions/18715345/how-to-create-a-user-for-postgres-from-the-command-line-for-bash-automation
+if [ -z "$PSQL_ROOT_PASSWORD" ]; then
+    echo "Setting psql root password to default pw"
+    sudo -i -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD 'password'"
+else
+    echo "Setting psql root password to $PSQL_ROOT_PASSWORD"
+    sudo -i -u postgres psql -c "ALTER ROLE postgres WITH PASSWORD '$PSQL_ROOT_PASSWORD'"
+fi
+
+# psql -h 127.0.0.1 -U postgres -W
+
+cat >> /var//www/html/tsugi/config.php << EOF 
+\$CFG->tool_folders = array("admin", "../tools", "mod");
+
+EOF
+
 echo ""
 if [ "$@" == "return" ] ; then
   echo "Tsugi PG4E Startup Returning..."
