6faab5d7a7ab4695bef81855f53bc872e530555f
Charles Severance <csev@umich.edu>
2020-05-10 16:16:20 -0400

    Updates

--- code/hidden-dist.py
+++ code/hidden-dist.py
@@ -2,11 +2,11 @@
 
 # https://www.pg4e.com/code/hidden-dist.py
 
-# psql -h pg.pg4e.com -p 5432 -U pg4e_user_8187f pg4e
+# psql -h pg.pg4e.com -p 5432 -U pg4e_be9e729093 pg4e_be9e729093
 
 # %load_ext sql
 # %config SqlMagic.autocommit=False
-# %sql postgresql://pg4e_user_8187f:pg4e_pass_7f749@35.222.5.70:13009/pg4e
+# %sql postgresql://pg4e_be9e729093:pg4e_p_d5fab7440699124@pg.pg4e.com:5432/pg4e_be9e729093
 # %sql SELECT 1 as "Test"
 
 def secrets():
@@ -24,13 +24,20 @@ def elastic() :
             "user": "pg4e_86f9be92a2",
             "pass": "2008_9d454b1f"}
 
+def readonly():
+    return {"host": "pg.pg4e.com",
+            "port": 5432,
+            "database": "pg4e_data",
+            "user": "pg4e_data_read",
+            "pass": "pg4e_p_d5fab7440699124"}
+
 # Return a psycopg2 connection string
 
 # import hidden
 # secrets = hidden.readonly()
 # sql_string = hidden.psycopg2(hidden.readonly())
 
-# 'dbname=pg4e_data user=pg4e_data_read password=pg4e_pass_94e5d host=35.239.113.162 port=10001'
+# 'dbname=pg4e_data user=pg4e_data_read password=pg4e_p_d5fab7440699124 host=pg.pg4e.com port=5432'
 
 def psycopg2(secrets) :
      return ('dbname='+secrets['database']+' user='+secrets['user']+
@@ -43,7 +50,7 @@ def psycopg2(secrets) :
 # secrets = hidden.readonly()
 # sql_string = hidden.alchemy(hidden.readonly())
 
-# postgresql://pg4e_data_read:pg4e_pass_94e5d@35.239.113.162:10001/pg4e_data
+# postgresql://pg4e_data_read:pg4e_p_d5fab7440699124@pg.pg4e.com:5432/pg4e_data
 
 def alchemy(secrets) :
     return ('postgresql://'+secrets['user']+':'+secrets['pass']+'@'+secrets['host']+
