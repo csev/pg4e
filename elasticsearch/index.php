4eac759023519b3186c8456c916ef9ef5f72a631
Charles Severance <csev@umich.edu>
2020-09-20 14:33:32 +0000

    Add some logging

--- elasticsearch/index.php
+++ elasticsearch/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Tsugi\Util\U;
+use \Tsugi\Core\LTIX;
 
 if ( ! defined('COOKIE_SESSION') ) define('COOKIE_SESSION', true);
 
@@ -14,6 +15,8 @@ function endsWith($haystack, $needle) {
 }
 }
 
+$p = $CFG->dbprefix;
+
 $pieces = U::rest_path();
 // echo("<pre>\n");var_dump($pieces);die();
 
@@ -21,7 +24,6 @@ if ( strlen($pieces->controller) < 1 || strlen($pieces->controller) < 1 ) {
     echo("<p>This provides access to elastic search indexes for use with this course.</p>");
     return;
 }
-
 // Check Basic Authentication
 $auth_user = U::get($_SERVER, 'PHP_AUTH_USER');
 $auth_pw = U::get($_SERVER, 'PHP_AUTH_PW');
@@ -35,30 +37,37 @@ if ( $es_pass != $auth_pw ) {
     return;
 }
 
+// Note the access
+$PDOX = LTIX::getConnection();
+$sql = "INSERT INTO ${p}elastic_access (index_name, updated_at) VALUES (:name, NOW())
+   ON DUPLICATE KEY UPDATE updated_at=NOW();";
+$values = array(":name" => $pieces->controller);
+$PDOX->queryDie($sql, $values);
+
 // Pull in all the data from the input request
-$apache_headers = apache_request_headers();
+$request_headers = apache_request_headers();
 $curl_headers = array();
-foreach($apache_headers as $key => $val ) {
+foreach($request_headers as $key => $val ) {
    $curl_headers[] = $key.": ".$val;
 }
 
 $base_url = $CFG->elasticsearch_backend;
-$method = $_SERVER['REQUEST_METHOD'];
+$request_method = $_SERVER['REQUEST_METHOD'];
 $tail = $pieces->controller;
 if ( $pieces->extra ) $tail .= '/' . $pieces->extra;
 $tail = U::reconstruct_query($tail, $_GET);
-$entityBody = file_get_contents('php://input');
-$es_url = $base_url . '/' . $tail;
+$request_body = file_get_contents('php://input');
+$request_url = $base_url . '/' . $tail;
 
 // Make the proxy request
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $es_url);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+curl_setopt($ch, CURLOPT_URL, $request_url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $request_method);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 1);
-if ( strlen($entityBody) > 0 ) {
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $entityBody);
+if ( strlen($request_body) > 0 ) {
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $request_body);
 }
 $response = curl_exec($ch);
 
@@ -66,38 +75,60 @@ $response = curl_exec($ch);
 // https://beamtic.com/curl-response-headers
 $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 $headers = substr($response, 0, $header_size);
-$body = substr($response, $header_size);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$response_body = substr($response, $header_size);
+$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-error_log("$method $tail input=".strlen($entityBody)." response code:".$httpCode." output=".strlen($body));
-if ( strlen($entityBody) > 0 ) {
-    error_log(substr($entityBody, 0, 100));
+if ( strlen($request_body) > 0 ) {
+    error_log(substr($request_body, 0, 100));
 }
 
 $headers_indexed_arr = explode("\r\n", $headers);
 $status_message = array_shift($headers_indexed_arr);
 // Create an associative array containing the response headers
-$headers_arr = array();
+$response_headers = array();
 foreach ($headers_indexed_arr as $value) {
   if(false !== ($matches = explode(':', $value, 2))) {
     if ( count($matches) != 2 ) continue;
-    $headers_arr["{$matches[0]}"] = trim($matches[1]);
+    $response_headers["{$matches[0]}"] = trim($matches[1]);
   }
 }
 
-// Send back the response
-http_response_code($httpCode);
+// Log the access
+error_log("$request_method $tail input=".strlen($request_body)." response code:".$response_code." output=".strlen($request_body));
+$sql = "INSERT INTO ${p}elastic_log
+    (index_name, auth_user, auth_pw,
+    request_url, request_body, request_headers,
+    response_code, response_body, response_headers)
+    VALUES
+    (:index_name, :auth_user, :auth_pw,
+    :request_url, :request_body, :request_headers,
+    :response_code, :response_body, :response_headers);";
+$values = array(
+    ":index_name" => $pieces->controller,
+    ":auth_user" => $auth_user,
+    ":auth_pw" => $auth_pw,
+    ":request_url" => $request_url,
+    ":request_body" => $request_body,
+    ":request_headers" => json_encode($request_headers),
+    ":response_code" => $response_code,
+    ":response_body" => $response_body,
+    ":response_headers" => json_encode($response_headers),
+);
+$PDOX->queryDie($sql, $values);
 
-foreach($headers_arr as $key => $value) {
+// Send back the response
+http_response_code($response_code);
+
+foreach($response_headers as $key => $value) {
    header($key . ': ' . $value);
 }
 
-echo($body);
+echo($response_body);
 
 
 /*
- 
+
  https://www.pg4e.com/elasticsearch/one/two/three/four
 
  object(stdClass)#5 (8) {
