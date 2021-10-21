54b587d8a759a1238994fdbea3a475a563e65cb7
Charles Severance <csev@umich.edu>
2021-10-21 23:21:09 +0000

    New expire strategy

---
+++ scripts/cl2_psql.py
@@ -0,0 +1,171 @@
+
+# Cleanup unused databases
+
+# python3 cleanup_psql.py
+
+import psycopg2
+import os
+import hidden
+import time
+import datetime
+import myutils
+import sys
+import math
+from email.message import EmailMessage
+
+limit = 10
+dryrun = True
+if len(sys.argv) == 2 and sys.argv[1] == "delete" : 
+    print('This is the real deal!')
+    dryrun = False
+
+# Load the secrets for the readwrite shared DB
+secrets = hidden.master()
+
+conn = psycopg2.connect(
+        host=secrets['host'],
+        port=secrets['port'],
+        # database=secrets['database'],
+        user=secrets['user'],
+        password=secrets['pass'],
+        connect_timeout=3)
+
+# https://stackoverflow.com/questions/34484066/create-a-postgres-database-using-python
+conn.autocommit = True
+cur = conn.cursor()
+
+sql = "select datname,oid from pg_database where datname='pg4e_025ca';"
+sql = "SELECT datname FROM pg_database;"
+# row = cur.execute(sql, fields)
+
+sql = "SELECT setting FROM pg_settings WHERE name = 'data_directory';"
+data_directory = myutils.queryValue(cur, sql)
+print('Data directory', data_directory)
+
+sql = "SELECT datname,oid FROM pg_database ORDER BY oid;"
+stmt = cur.execute(sql)
+
+expired = list()
+conn2 = False
+cur2 = False
+keep = 0
+while True :
+    if len(expired) > limit : break
+    row = cur.fetchone() 
+    if not row : break
+    db_name = row[0]
+    if db_name.startswith('pg4e_data') : continue
+    if not db_name.startswith('pg4e_') : continue
+    db_oid = row[1]
+    # time.sleep(1)
+
+    if cur2 is not False:
+        try:
+            cur2.close()
+        except:
+            pass
+        cur2 = False
+    if conn2 is not False:
+        try:
+            conn2.close()
+        except:
+            pass
+        conn2 = False
+
+    try:
+        conn2 = psycopg2.connect(
+            host=secrets['host'],
+            port=secrets['port'],
+            database=db_name,
+            user=secrets['user'],
+            password=secrets['pass'],
+            connect_timeout=3)
+
+        cur2 = conn2.cursor()
+        sql = "SELECT valstr, created_at, updated_at  FROM pg4e_meta WHERE keystr='access';"
+        stmt = cur2.execute(sql)
+        row = cur2.fetchone()
+    except:
+        print(db_name, "Schema fail")
+        expired.append((db_name, "Schema fail",0,0))
+        continue
+
+    if row == False or len(row) != 3 :
+        print(db_name, "p4e_meta missing")
+        expired.append((db_name, "p4e_meta missing",0,0))
+        continue
+
+    created_at = row[1]
+    updated_at = row[2]
+
+    # https://stackoverflow.com/questions/1345827/how-do-i-find-the-time-difference-between-two-datetime-objects-in-python
+    now_at=datetime.datetime.now()
+    if created_at is None:
+        c_days = 999
+    else:
+        ts_diff=now_at-created_at
+        c_days=int(ts_diff.total_seconds() / (60*60*24))
+    if updated_at is None:
+        u_days = 999
+    else:
+        ts_diff=now_at-updated_at
+        u_days=int(ts_diff.total_seconds() / (60*60*24))
+
+    # print(db_name, c_days, u_days)
+
+    if c_days < 120 : 
+        print(db_name, "keep create",c_days,u_days)
+        keep = keep + 1
+        continue
+    if u_days < 60 : 
+        print(db_name, "keep update",c_days,u_days)
+        keep = keep + 1
+        continue
+
+    print(db_name, "expired",c_days,u_days)
+    expired.append((db_name, "expired",c_days,u_days))
+
+if cur2 is not False:
+    try:
+        cur2.close()
+    except:
+        pass
+    cur2 = False
+if conn2 is not False:
+    try:
+        conn2.close()
+    except:
+        pass
+    conn2 = False
+
+print('Keep', keep, 'expire', len(expired))
+
+if not dryrun :
+    print("Here we go - going to delete", len(expired))
+    time.sleep(5)
+
+print()
+actions = list()
+for db in expired:
+    sql = 'DROP DATABASE '+db[0]+';'
+    actions.append(sql+' '+db[1]+' -- create days='+str(db[2])+' update_days='+str(db[3]))
+    if not dryrun:
+        print(sql)
+        time.sleep(1)
+        try:
+            stmt = cur.execute(sql)
+        except Exception as e:
+            actions.append(str(e))
+            print(e)
+
+cur.close()
+
+# Send some email
+if len(actions) > 0 :
+    message = "Subject: Postgres Expire Actions ("+str(len(actions))+")\n\n"
+    if dryrun: message = message + "This is a dry run\n\n";
+    for action in actions:
+        message = message + action + "\n";
+    print(message)
+    myutils.sendMail(message)
+
---
+++ scripts/cl2_psql.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Running cl2_psql.py"
+
+cd /home/csev
+
+python3 cl2_psql.py delete
+
