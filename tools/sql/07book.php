d7223922159bc4c1f2cdc51bb4317c4202d80f66
Anthony Whyte <anthwhyte@gmail.com>
2022-02-04 22:01:19 -0500

    Restrict pip install elasticsearch command to version 7.13.x

--- tools/sql/07book.php
+++ tools/sql/07book.php
@@ -137,9 +137,18 @@ If necessary, copy <b>hidden-dist.py</b> to <b>hidden.py</b> and put your Elasti
 into the <b>elastic()</b> method.  You should also put your PostgreSQL secrets into this file as well.
 </p>
 <p>
-You will need to install the Python Elasticsearch library if you have not already done so.
+You will need to install the Python Elasticsearch library (version 7.13.x) if you have not already done so.
 <pre>
-pip install elasticsearch
+pip install 'elasticsearch<7.14.0'
+</pre>
+</p>
+<p>
+If installing elasticsearch locally run <b>pip</b> with the <b>-m</b> flag enabled:
+<pre>
+macOS: python3 -m pip install 'elasticsearch<7.14.0'
+</pre>
+<pre>
+Windows: python -m pip install 'elasticsearch<7.14.0'
 </pre>
 </p>
 <p>
