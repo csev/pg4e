f83cc9931f946ebf88f37afd103492075780f379
Charles Severance <csev@umich.edu>
2025-07-05 09:28:24 -0400

    Updates

--- code/rosetta.js
+++ code/rosetta.js
@@ -1,23 +1,23 @@
 // JavaScript: rosetta.js
 
-x = {key : 'value', lines: 25 }
+const x = {key : 'value', lines: 25 }
 console.log(x)
-y = ['bob', 'alice']
+const y = ['bob', 'alice']
 console.log(y)
 
-a = 17;
+const a = 17;
 if ( a < 20 ) {
     console.log('Below 20')
 } else {
     console.log('20 or higher')
 }
 
-for(i=0; i<5; i++) {
+for(var i=0; i<5; i++) {
   console.log(i);
 }
 
 console.log('/a/b/c'.split('/'))
 
-c = 'hello' + '\n' + 'world'
+const c = 'hello' + '\n' + 'world'
 console.log(c)
 
