062a395162eb0388b42596dbd38337587dc66f60
Charles Severance <csev@umich.edu>
2026-02-21 10:54:48 -0500

    Accessibility

--- foot.php
+++ foot.php
@@ -1,7 +1,7 @@
 <?php
 
 $foot = '
-<p style="font-size: 75%; margin-top: 5em;">
+<p style="font-size: 0.875rem; color: #333; margin-top: 5em;">
 Copyright Creative Commons Attribution 4.0
 </p>';
 
